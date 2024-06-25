358062f0870570561af9ec226d6c596e1200e12d
beautifulstarfish <164958630+beautifulstarfish@users.noreply.github.com>
2024-06-25 19:37:52 -0400

    Edit files according to lecturer

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # brainstation-intro-workshop
+
+This workshop tests the foundational knowledge on HTML, CSS, and Javascript. Some of the skills I have demonstrated include:
+
+- Follow along and test the various languages that will be assessed during admissions.
+- Edit navigation bar links.
+- Edit image placement on webpage.
+- Edit style property for featured recipe.
+- Test JS.
---
+++ javascript-exercise.js
@@ -0,0 +1,21 @@
+// Variables & Comparisons
+
+/*
+    Step 1
+    In a variable named "isRaining", store a true/false value representing the weather right now.
+*/
+
+let isRaining = true
+
+/*
+    Step 2
+    Using the result variable, write an if/else statement to check if it's raining.
+    If it is raining, console.log "Get an umbrella" 
+    If it isn't raining, console.log "Beautiful sunshine!"
+*/
+
+if (isRaining) {
+    console.log('Get an umbrella')
+} else {
+    console.log('Beautiful sunshine!')
+}
